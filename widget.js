43380281fbd4243b84c667a4b5353c692d2c71af
JB <jbpiot@gmail.com>
2014-02-19 17:24:06 +0100

    Update widget.js to new template

--- widget.js
+++ widget.js
@@ -4,67 +4,64 @@
  * @author JB Piot
  */
 
-WAF.define('ColorPicker', function () {
-    var Widget = WAF.require('waf-core/widget');
-    var widget = Widget.create('ColorPicker');
-    
-    /**
-    * Framework creates the widget
-    */
-    widget.prototype.init = function () {
-        
-        $(this.node).empty();
-        
-        var myPicker  = document.createElement('input');
-        var thisWidget = this;
-        
-        $(this.node).append(myPicker);
-        
-        $(myPicker).spectrum({
-            color: "#000000",
-            change: function (color) {
-                console.log('color change', color);
-                thisWidget.color (color.toHexString());
-                thisWidget.red   (color.toRgb().r);
-                thisWidget.green (color.toRgb().g);
-                thisWidget.blue  (color.toRgb().b);
-			}
-        });
-    };
+WAF.define('ColorPicker', ['waf-core/widget'], function (widget) {
 
-    /**
-    * generate a function to set or get a specified component
-    */
-    var componentSetter = function(component) {
-        return function(value) {
-            console.log(this.id, component, value);
+    var componentSetter = function (component) {
+        return function (value) {
+            //console.log(this.id, component, value);
             var color = $("input", this.node).spectrum("get").toRgb();
-            
+
             color[component] = value;
             var s = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
-            
+
             $("input", this.node).spectrum("set", s);
         };
     };
-    
-    widget.addProperty('red', {
-        defaultValue: 0,
-        onChange: componentSetter('r')
-    });
-    widget.addProperty('green', {
-        defaultValue: 0,
-        onChange: componentSetter('g')
-    });
-    widget.addProperty('blue', {
-        defaultValue: 0,
-        onChange: componentSetter('b')
-    });
-    widget.addProperty('color', {
-        defaultValue: '#000000',
-        onChange: function(v) {
-            $("input", this.node).spectrum("set", v);
-        }
+
+    var ColorPicker = widget.create('ColorPicker', {
+        init: function () {
+
+            $(this.node).empty();
+
+            var myPicker = document.createElement('input');
+            var thisWidget = this;
+
+            $(this.node).append(myPicker);
+
+            $(myPicker).spectrum({
+                color: "#000000",
+                change: function (color) {
+                    //console.log('color change', color);
+                    thisWidget.color(color.toHexString());
+                    thisWidget.red(color.toRgb().r);
+                    thisWidget.green(color.toRgb().g);
+                    thisWidget.blue(color.toRgb().b);
+                }
+            });
+        },
+        'red': widget.property({
+            defaultValue: 0,
+            bindable: false,
+            onChange: componentSetter('r')
+        }),
+        'green': widget.property({
+            defaultValue: 0,
+            bindable: false,
+            onChange: componentSetter('g')
+        }),
+        'blue': widget.property({
+            defaultValue: 0,
+            bindable: false,
+            onChange: componentSetter('b')
+        }),
+        'color': widget.property({
+            defaultValue: '#000000',
+            onChange: function (v) {
+                //console.log(this.id, 'color', v);
+                $("input", this.node).spectrum("set", v);
+            }
+        })
     });
 
-	return { ColorPicker: widget };
+    return ColorPicker;
 });
