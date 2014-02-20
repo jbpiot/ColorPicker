eccf91b7002240391dc1c9686254ee75f23a5d20
JB <jbpiot@gmail.com>
2014-02-20 11:21:36 +0100

    Disabling the event handlers in the designer

--- designer.js
+++ designer.js
@@ -1,10 +1,10 @@
 /**
  * ColorPicker widget for Wakanda
- * With the help of an external library, you can include a color picker on your Wakanda Pages. 
+ * With the help of an external library, you can include a color picker on your Wakanda Pages.
  * @author JB Piot
  */
 (function (ColorPicker) {
-    ColorPicker.setDescription('Color Picker');  //Widget's display name
+    ColorPicker.setDescription('Color Picker'); //Widget's display name
 
     /*Default size*/
     ColorPicker.setWidth('50');
@@ -20,4 +20,22 @@
         'label': true,
         'disabled': ['border-radius']
     });
+
+    var initSave = ColorPicker.prototype.init;
+
+    ColorPicker.prototype.init = function () {
+
+        initSave.apply(this);
+
+        unbindChildren($(this.node));
+
+        function unbindChildren(parent) {
+            $(parent).children().each(
+                function (i) {
+                    unbindChildren($(this));
+                    $(this).undelegate();
+                });
+        }
+    }
+
 });
